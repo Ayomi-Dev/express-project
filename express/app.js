7627eff1caad0e53a9830ec110d5c470065e77d7
Ayomide <ayomideolakunlea1@gmail.com>
2024-09-02 18:01:42 +0100

    linked express app to mongoDB using mongoose

--- express/app.js
+++ express/app.js
@@ -1,163 +1,139 @@
 // importing the express module
 const express = require('express');
 
-// seeting up middleware module
+// setting up middleware module
 const morgan = require('morgan');
 
+// importing mongoose
+const mongoose = require('mongoose')
 
+// importing database collection
+const Verse = require('../models/verses')
 // setting up an express app
 const app = express();
 
 // importing file system module
 const fs = require('fs');
 
+// setting up database to store verses
+const dbURI = "mongodb+srv://ayomideolakunlea1:nodeproject1234.@node-project.7gmk7.mongodb.net/verse-db?retryWrites=true&w=majority&appName=node-project"
+mongoose.connect(dbURI)
+.then(result => {
+    console.log('connected to db')
+    app.listen(3000)
+})
+.catch(err => console.log(err))
 
 // registering view engine to serve up dynamic data to html template
 app.set('view engine', 'ejs')
 
 // setting up the path module for easy routing
 const path = require('path');
-const { render } = require('ejs');
 
-// listening for request
-app.listen(3000)
 
 // creating a custom middle to run just on the server
-// app.use((req, res, next) => {
-//     console.log('new req made')
-//     console.log(req.path)
-//     console.log(req.method);
 
-//     next()
-// })
+app.use(morgan('dev')); // creating a third-party middleware
 
-// creating a third-party middleware
-app.use(morgan('dev'));
+app.use(express.urlencoded({extended: true})) // setting up middleware for form submission
+app.use(express.json())  //allows express to send/receive json data
 
-// setting up middleware for form submission
-app.use(express.urlencoded({extended: true}))
 
 // routing using express
 
-// serve static files from a directory
-app.use(express.static('public'));
+app.use(express.static('public')); // serve static files from a directory
 
-// serve up dynamic files from a view directory
-app.set('views', path.join(__dirname, '../views'));
+app.set('views', path.join(__dirname, '../views')); // serve up dynamic files from a view directory
+
+
+app.get('/new-verse', (req, res) => {
+    const verse = new Verse({
+        chapter: 'psalms18',
+        verse: 28,
+        text: 'My darkness is enlightened'
+    })
+    
+})
+
+app.get('/all-verses', (req, res) => {
+    Verse.find() //queries the db and returns all documents in the collection
+    .then(result => {
+        res.send(result)
+    })
+})
+
+app.get('/verse', (req, res) => {
+    Verse.findById("66d5c768dad3a7b12c083220") //queries the db and returns a document that matches the id pased in
+    .then(result => {
+        res.send(result)
+    })
+    .catch(err => console.log(err))
+})
 
 
 app.get('/', (req, res) => {
-    // res.send('<p>about home page</p>')
-    // sending a static file to the browser
-    // res.sendFile('index.html', {root:path.join(__dirname, '../pages')});
-
-    // creating data to pass into ejs file
-    const verses = JSON.parse(fs.readFileSync('./public/verses.json'));
-    // sending dynamic file to the browser, we use the render() method
-    res.render('index', {title: 'Home', verses})
+    res.redirect('/verses')
 });
 
+app.get('/verses', (req, res) => {
+    Verse.find().sort({ createdAt: -1 }) //queries the db and returns all documents in the collection from newest to oldest
+    .then((result) => {
+        res.render('index', {title: 'Home', verses: result})
+    })
+})
 
 app.get('/about', (req, res) => {
     res.render('about', {title: 'about'}) ;
 }); 
 
-// posting a request to a server to write a new data to a file
+// posting a request to a server to create a new document and add to a database
 app.get('/verse/create', (req, res) => {
-    res.render('create', {title: 'create', message: null})
+
+    res.render('create', {title: 'create a verse', message: null})
 });
 
-app.post('/verse/create', (req, res) => {
-    const verses = JSON.parse(fs.readFileSync('./public/verses.json'));
+app.post('/verses', (req, res) => {
+    const verse = new Verse( req.body )
 
-    const newVerse = {
-        chapter: req.body.chapter,
-        verse: req.body.verse,
-        text: req.body.text
-    }
-    verses.push(newVerse);
+    verse.save() //saves the new created document to the db collection
+    .then(result => {
+        res.redirect('/');
+    })
+    .catch(err => console.log(err));
     
-
-        // writing new data in the json file
-    fs.writeFile('./public/verses.json', JSON.stringify(verses, null, 2), (err) => {
-        // res.json({success: true, message:'Verse added succesfully', redirect: '/'});
-        if(err){
-            console.log('Internal err:', err);
-            res.status(500).json({success: false, message:'Internal server error'});
-         }
-        else{
-            console.log(verses)
-            res.redirect('/')
-            // res.json({success: true, message:'Verse added succesfully', redirect: '/'});
-        }
-    });
 });
 
-// redirects using express
-// app.get('/about-us', (req, res) => {
-//     res.redirect('/about');
-// })
+
 
 // response for a specific or selected data
-app.get('/verse/:chapter', (req, res) => {
-    const verses = JSON.parse(fs.readFileSync('./public/verses.json'))
-    const verse = verses.find(verse => verse.chapter === (req.params.chapter));
-    
-    res.render('verse', {title: `${verse.chapter}:${verse.verse}`, verse})
+app.get('/verses/:id', (req, res) => {
+    // console.log(req.params.id)
+    Verse.findById(req.params.id) //queries the db and returns a document that matches the id pased in
+    .then(result => {
+        const verse = result
+        res.render('verse', {title: `${verse.chapter}:${verse.verse}`, verse})
+    })
+    .catch(err => console.log(err))
     
 })
 
 // handling form submission----posting a request to the server
 app.get('/verse/:chapter/edit', (req, res) => {
-    const verses = JSON.parse(fs.readFileSync('./public/verses.json'))
-    const verse = verses.find(verse => verse.chapter === req.params.chapter)
-    res.render('edit', {title: 'Edit', verse})
+    
 });
 
 app.post('/verse/:chapter/edit', (req, res) => {
-    const verses = JSON.parse(fs.readFileSync('./public/verses.json'));
-
-    // accessing and manipulating data of a selected item in a list
-    const verseIndex = verses.findIndex(verse => verse.chapter === req.params.chapter);
-    if(verseIndex !== -1){
-
-        // console.log('Recieved form', req.body)
-
-        verses[verseIndex].text = req.body.text
-        
-        // import file module to update changes made to the selected data
-        
-        fs.writeFile('./public/verses.json', JSON.stringify(verses, null, 2), (err) =>{
-            if(err){
-                console.log('eeror writing to file',err)
-                res.status(500).send('internal server error')
-            }
-            else{
-                res.redirect(`/`);
-            }
-        })
-
-    }
-    else{
-        res.status(404).redirect('404')
-    }
+    
+    
 })
 
 // deleting data from a file
-app.delete('/verse/:chapter', (req, res) => {
-    let verses = JSON.parse(fs.readFileSync('./public/verses.json'));
-    const updatedVerses = verses.filter(verse => verse.chapter !== req.params.chapter);
-    
-
-    fs.writeFile('./public/verses.json', JSON.stringify(updatedVerses, null, 2), (err)=>{
-        if(err){
-            console.log(err)
-            res.status(500).json({success: false, message:'Internal server error'});
-        }
-        else{
-            res.json({success: true, message: 'Post deleted successfully', redirect:'/'})
-        }
+app.delete('/verses/:id', (req, res) => {
+    Verse.findByIdAndDelete(req.params.id) //deletes a document that matches the id passed in
+    .then(result => {
+        res.json({redirect: '/', message: 'verse deleted'})
     })
+    .catch(err => console.log(err))
 })
 
 
